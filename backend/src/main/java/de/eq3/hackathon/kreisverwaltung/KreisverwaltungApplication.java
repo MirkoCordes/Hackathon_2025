321ae7943ed9ce75927322f2c77d527fd676f651
Niklas Friedrich <niklas.friedrich@entwicklung.eq-3.de>
2025-09-26 10:40:37 +0200

    Update pom

--- backend/src/main/java/de/eq3/hackathon/kreisverwaltung/KreisverwaltungApplication.java
+++ backend/src/main/java/de/eq3/hackathon/kreisverwaltung/KreisverwaltungApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KreisverwaltungApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting Kreisverwaltung Application...");
 		SpringApplication.run(KreisverwaltungApplication.class, args);
 	}
 
