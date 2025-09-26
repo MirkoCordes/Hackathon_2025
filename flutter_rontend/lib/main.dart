fd98e49a204db481136d40ed5e6f8cdb3a3c1734
Mirko Cordes <mirko.cordes@stud.hs-emden-leer.de>
2025-09-26 11:26:27 +0200

    basic dependencies

--- flutter_rontend/lib/main.dart
+++ flutter_rontend/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Landkreis - Kreisverwaltung',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,9 +28,11 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 144, 196, 23),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Landkreis - Kreisverwaltung'),
     );
   }
 }
