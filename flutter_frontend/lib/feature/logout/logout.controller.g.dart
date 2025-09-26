3fec8a3fec680331822449585e8ddf9e401717f8
Quentin Voß <quentinvoss111@gmail.com>
2025-09-26 17:36:42 +0200

    added logout button

---
+++ flutter_frontend/lib/feature/logout/logout.controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'logout.controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$logoutControllerHash() => r'90c091338500d358f2deb0f6f0537e0f18c18c20';
+
+/// See also [LogoutController].
+@ProviderFor(LogoutController)
+final logoutControllerProvider =
+    AutoDisposeNotifierProvider<LogoutController, void>.internal(
+      LogoutController.new,
+      name: r'logoutControllerProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$logoutControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$LogoutController = AutoDisposeNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
